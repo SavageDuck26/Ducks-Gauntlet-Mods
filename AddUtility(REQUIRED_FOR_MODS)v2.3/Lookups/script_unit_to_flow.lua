812d20742ec80fb28b0cae034f8f9573f9794348
Duck <SavageDuck26@protonmail.com>
2026-07-21 08:06:50 -0400

    Delete old vers

--- AddUtility(REQUIRED_FOR_MODS)v2.3/Lookups/script_unit_to_flow.lua
+++
@@ -1,9 +0,0 @@
--- =================================================================================================
--- Author: SavageDuck26
--- Purpose: All flow events to the units that hold them for usage.
--- =================================================================================================
-
-_G.UnitFlowLookup = {
-    ["ultimate_remnant"] = "ultimate_remnant_windup",
-    
-}
